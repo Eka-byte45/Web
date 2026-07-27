35e61146129315f56f3a817aefbc0523d6448f83
Екатерина Голумбиевская <golumbiyevskaya90@bk.ru>
2026-07-28 01:00:41 +0800

    Switch background delay DONE

--- JavaScript/forms_and_events.js
+++ JavaScript/forms_and_events.js
@@ -78,3 +78,13 @@ function switchBackground(e)
     ////switchButton.src = 'sun.png';
     //document.getElementById("debug-background").innerHTML = document.body.className;
 }
+document.getElementById("switch-background-delay").addEventListener("change",setDelay);
+function setDelay(e)
+{
+    let delay = e.target.value;
+        document.getElementById('switch-background').style.transition = 
+        document.body.style.transition =
+        `color ${delay},background-color ${delay}, background-image ${delay}`;
+    console.table(document.body.style);
+    console.table(document.getElementById(`#switch-backgroung`).style);
+}
